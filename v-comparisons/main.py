a015f21bc455cd0f5c63e76f986b1182e818bbe0
petarb33 <petarbokun1@gmail.com>
2025-10-14 08:45:26 +0200

    style: remove spacing

--- v-comparisons/main.py
+++ v-comparisons/main.py
@@ -1,14 +1,10 @@
-from fetch import get_input, get_data, get_drivers, get_event_info
+from fetch import get_input, get_data, get_event_info
 from process import get_drivers_max_speeds, get_drivers_mean_speeds, get_drivers_min_speeds
 from plot import plot_speed_comparisons
 
 def main():
     country, year, session = get_input()
-
     session_data, event = get_data(country, year, session)
-
-    drivers = get_drivers(session_data)
-
     event_info = get_event_info(session_data, event)
 
     min_speeds = get_drivers_min_speeds(session_data)
