cef0825fbec4a7d3e582885532d885f029ad029e
petarb33 <petarbokun1@gmail.com>
2025-10-24 12:13:36 +0200

    docs: update and add docstrings for FastF1 utility functions

--- quali-telemetry-comparisons/data_fetching.py
+++ quali-telemetry-comparisons/data_fetching.py
@@ -6,18 +6,72 @@ from typing import Tuple, List, Dict
 from f1_types import SessionConfig
 
 def get_input() -> Tuple[str, int, List[str]]:
-    """Load the session config and return country and year"""
+    """"
+    Load the session configuration and return the country, year and drivers list.
+
+    This function reads the session configuration from a local JSON file
+    using `load_session` and extracts the `country`, `year` and `drivers` fields.
+
+    Returns
+    -------
+    tuple of (str, int)
+        A tuple containing:
+        - country : str
+            The name of the country for the race weekend.
+        - year : int
+            The year of the event.
+        - drivers : str
+            Drivers abbreviations whose laps we compare.
+    """
     config = load_session()
     return config['country'], config['year'], config['drivers'].split()
 
 def load_session() -> SessionConfig:
-    """Load the session configuration from a JSON file."""
+    """
+    Load the session configuration from a JSON file.
+    
+    Reads the 'session.json' file located in the current working directory,
+    that contains 'country', 'year' and 'drivers' keys.
+
+    Returns
+    -------
+    dict
+        Dictionary containing session configuration values.
+        - 'country' : str
+            Race country names, alternative names also work (e.g., "Miami"),
+            since there are multiple races in same countries (e.g., "United States").
+        - 'year' : int
+            Year of the race weekend
+        - 'drivers' : str
+            Drivers abbreviations whose laps we compare.
+    """
     file_path = 'session.json'
     with open(file_path, 'r') as f:
         return json.load(f)
 
 def get_data(country: str, year: int) -> Tuple[Session, Event]:
-    """Fetch the FastF1 session and event data."""
+    """
+    Fetch and load FastF1 session and event data for given input.
+
+    This functions uses FastF1 Api to get specific session data
+    and the corresponding event information.
+
+    Parameters
+    ----------
+    country : str
+        Name of the country where the race is held (e.g., "Monaco")
+    year : int
+        Year of the race weekend.
+
+    Returns
+    -------
+    tuple of (Session, Event)
+        A tuple containing:
+        - session_data : fastf1.core.Session
+            Loaded FastF1 session object.
+        - event : fastf1.events.Event
+            Event object.
+    """
     session_data = fastf1.get_session(year, country, 'Q')
     session_data.load()
     event = fastf1.get_event(year, country)
@@ -25,6 +79,35 @@ def get_data(country: str, year: int) -> Tuple[Session, Event]:
     return session_data, event
 
 def get_event_info(session_data: Session, event: Event) -> Dict[str, str | int]:
+    """
+    Extract structured information about the race weekend.
+
+    Parameters
+    ----------
+    session_data : fastf1.core.Session
+        FastF1 session object with session information.
+    event : fastf1.events.Event
+        FastF1 event object with general race weekend information.
+
+    Returns
+    -------
+    dict
+        Dictionary with information about the whole weekend:
+        - 'grand_prix' : str
+            Official Grand Prix Name.
+        - 'location' : str
+            Circuit short name.
+        - 'country_name' : str
+            Name of the country.
+        - 'country_code' : str
+            ISO Country code.
+        - 'round_number' : int
+            Round number
+        - 'session' : str
+            Session type (e.q., 'Practice 1')
+        - 'year' : int
+            Year of the race weekend
+    """
     return {
         'grand_prix' : session_data.session_info['Meeting']['Name'],
         'location' : session_data.session_info['Meeting']['Circuit']['ShortName'],
